8ff4f2bf0781fd2477b4eace83e9dd731bd5c625
Alreynn <aliung04@gmail.com>
2025-12-22 11:51:23 +0700

    Changed footer size, for convenience.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,15 +4,15 @@ export const Footer = () => {
     return (
         <footer className="fixed flex justify-evenly w-full bottom-0 z-20 backdrop-blur-[8px] border-t-[1px] p-2">
             <a href="https://github.com/Alreynn/prabmul-media-player" className="flex relative flex-col items-center w-56">
-                <Telescope className="size-[2em] -mb-1 transition-all active:scale-110" />
+                <Telescope className="size-[1.5em] -mb-1 transition-all active:scale-110" />
                 <p>Orisinil</p>
             </a>
             <button onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className="flex relative flex-col items-center w-56">
-                <House className="size-[2em] -mb-1 transition-all active:scale-110" />
+                <House className="size-[1.5em] -mb-1 transition-all active:scale-110" />
                 <p>Halaman</p>
             </button>
             <a href="https://github.com/Alreynn/prabmul-media-player-R" className="flex flex-col items-center w-56">
-                <CodeXml className="size-[2em] -mb-1 transition-all active:scale-110" />
+                <CodeXml className="size-[1.5em] -mb-1 transition-all active:scale-110" />
                 <p>Kode Sumber</p>
             </a>
         </footer>
