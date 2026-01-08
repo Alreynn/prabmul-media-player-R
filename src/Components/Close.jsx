d06f93701f10e23b4b10a1488897d57b8cf85f54
Alreynn <aliung04@gmail.com>
2026-01-08 13:40:29 +0700

    Added scrollLock if floatPlayer is opened

--- src/Components/Close.jsx
+++ src/Components/Close.jsx
@@ -4,10 +4,11 @@ export const Closed = () => {
     useEffect(() => {
         document.body.style.overflow = 'hidden';
     }, [])
+    
     return (
         <div className="fixed flex flex-col items-center justify-center bg-zinc-900 h-screen w-full z-[100]">
             <h1 className="text-3xl font-bold">Prabmul Media Player</h1>
-            <p className="text-lg">Situs ditutup untuk beberapa hari.</p>
+            <p className="text-lg">Ditutup untuk sementara waktu.</p>
             <p className="text-sm">Perlu sedikit perbaikan.</p>
         </div>
     )
